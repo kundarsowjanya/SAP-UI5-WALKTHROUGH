a58e8230f418f2e430b759e5d4a034e07f76fcbf
I565171 <g.sowjanya@sap.com>
2023-05-28 14:41:02 +0530

    Modules added

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],function(Controller){
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+],function(Controller,MessageToast){
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough",{
         onShowHello: function(){
-        alert("Hi everyone")
+            MessageToast.show("Hi Everyone")
         }
 
     })
