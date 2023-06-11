3d3b191b55d24b4071cea522ed33d4a811c2d08c
I565171 <g.sowjanya@sap.com>
2023-06-11 12:18:47 +0530

    Dialog and Fragments

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-],function(Controller,MessageToast){
+    "sap/ui/core/Fragment"
+],function(Controller,MessageToast,Fragment){
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.App", {
     
@@ -10,6 +11,21 @@ sap.ui.define([
             var sRecipient=this.getView().getModel().getProperty("/recipient/name");
             var sMsg=oBundle.getText("helloMsg",[sRecipient]);
             MessageToast.show(sMsg)
+        },
+        onOpenDialog:function(){
+             var oView=this.getView()
+             if(!this.byId("helloDialog")){
+                Fragment.load({
+                    id:oView.id,
+                    name:"sap.ui.demo.walkthrough.view.HelloDialog"
+                }).then(function(oDialog){
+                    //connect Dialog to the root view
+                    oView.addDependent(oDialog);
+                    oDialog.open();
+                })
+             }else{
+                 this.byId("hellDialog").open();
+             }
         }
 
     })
